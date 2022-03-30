70356c97dc69b08c62b3c1d3ad6310512a427e62
Vitaly Borodulin <rainbowf@mail.ru>
2022-03-31 00:24:08 +0500

    Register models in admin

--- foodplan_bot/admin.py
+++ foodplan_bot/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Subscription, DishRecipe
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'tg_chat_id', 'phone_number')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'menu_type',
+        'person_count',
+        'eating_count',
+        'allergy',
+        'expiration_date',
+    )
+
+
+@admin.register(DishRecipe)
+class DishRecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'image',
+        'name',
+        'ingredients',
+        'description',
+        'calorific_value',
+        'recipe',
+        'subscriptions',
+    )
